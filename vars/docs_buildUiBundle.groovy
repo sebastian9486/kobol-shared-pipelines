b2f1f2a1da1e5d6048cb648c1518c314c1fd9148
sebastian <sommerfeld.sebastian@gmail.com>
2021-01-15 14:36:55 +0100

    minor update

--- vars/docs_buildUiBundle.groovy
+++ vars/docs_buildUiBundle.groovy
@@ -7,7 +7,7 @@
  *************************************/
 
 def call(body) {
-    
+
     // evaluate the body block, and collect configuration into the object
     def pipelineParams= [:]
     body.resolveStrategy = Closure.DELEGATE_FIRST
